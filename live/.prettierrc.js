c4ecbc0eb3a266e1c37b688b5ec473aa53f95692
Illia Olenchenko <icrosil@gmail.com>
2020-08-24 17:40:20 +0300

    added prettier

---
+++ live/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  printWidth: 120
+};
