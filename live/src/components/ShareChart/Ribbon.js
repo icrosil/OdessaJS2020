dfd58b37957b4bdcf9cdcbf2747244d60f8b7936
Illia Olenchenko <icrosil@gmail.com>
2020-09-30 00:06:06 +0300

    last updates

--- live/src/components/ShareChart/Ribbon.js
+++ live/src/components/ShareChart/Ribbon.js
@@ -1,28 +1,49 @@
 import React, { useState } from 'react';
 import * as PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { arc as arcD3 } from 'd3-shape';
 
 import useStyles from './styles';
 
-// function degreeToRadian(degree) {
-//   return (degree / 180) * Math.PI;
-// }
+import { LinearGradient } from '../SVG/LinearGradient';
 
-export function Ribbon({ chords, ribbon }) {
+function degreeToRadian(degree) {
+  return (degree / 180) * Math.PI;
+}
+
+export function Ribbon({ chords, ribbon, color, innerRadius }) {
   const classes = useStyles();
-  const [, setMouseOverChord] = useState(null);
+  const [mouseOverChord, setMouseOverChord] = useState(null);
+  const arc = arcD3().innerRadius(innerRadius).outerRadius(innerRadius);
 
   return (
     <g>
       {chords.map(chord => {
+        const sourceAngle = arc.centroid(chord.source);
+        const targetAngle = arc.centroid(chord.target); // [startAngle, endAngle]
+        const [x1, y1] = sourceAngle.map(degreeToRadian);
+        const [x2, y2] = targetAngle.map(degreeToRadian);
         const chordId = `${chord.source.index}-${chord.target.index}`;
+        const linearGradientId = `gradient-${chordId}`;
         return (
           <g key={chordId}>
+            <LinearGradient
+              x1={x1 * innerRadius}
+              x2={x2 * innerRadius}
+              y1={y1 * innerRadius}
+              y2={y2 * innerRadius}
+              id={linearGradientId}
+              colorFrom={color(chord.source.index)}
+              colorTo={chord.target.index}
+            ></LinearGradient>
             <path
               onMouseOver={() => setMouseOverChord(chordId)}
               onMouseOut={() => setMouseOverChord(null)}
-              className={clsx(classes.ribbon)}
+              className={clsx(classes.ribbon, {
+                [classes.ribbonActive]: mouseOverChord === chordId,
+              })}
               d={ribbon(chord)}
+              fill={`url(#${linearGradientId})`}
             />
           </g>
         );
